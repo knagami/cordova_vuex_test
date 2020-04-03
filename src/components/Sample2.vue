0045a73be8d0f2d22d8dd5e40a25f03eff021369
knagami <knagami008@yahoo.co.jp>
2020-04-03 16:25:54 +0900

    feat:.env追加

--- src/components/Sample2.vue
+++ src/components/Sample2.vue
@@ -26,15 +26,6 @@
               label="type"
             ></v-select>
             <v-select
-              v-model="mode"
-              :items="modes"
-              dense
-              outlined
-              hide-details
-              label="event-overlap-mode"
-              class="ma-2"
-            ></v-select>
-            <v-select
               v-model="weekday"
               :items="weekdays"
               dense
@@ -74,19 +65,16 @@
 import Vue from "vue";
 import store from "@/store.js";
 import { mapState, mapGetters } from 'vuex'
-// import the plugin
 import VueGAPI from "vue-gapi";
 
-// create the 'options' object
+
+
 const apiConfig = {
-    apiKey: "xxxx",
-    clientId: "yyyy",
+    apiKey: process.env.VUE_APP_apiKey,
+    clientId: process.env.VUE_APP_clientId,
     discoveryDocs: ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"],
     scope: "https://www.googleapis.com/auth/calendar"
-    // see all available scopes here: https://developers.google.com/identity/protocols/googlescopes'
 };
-
-// Use the plugin and pass along the configuration
 Vue.use(VueGAPI, apiConfig);
 
 export default {
@@ -105,45 +93,37 @@ export default {
             { text: 'Mon, Wed, Fri', value: [1, 3, 5] },
         ],
         value: '',
-        //events: [],
         colors: ['blue', 'indigo', 'deep-purple', 'cyan', 'green', 'orange', 'grey darken-1'],
         names: ['Meeting', 'Holiday', 'PTO', 'Travel', 'Event', 'Birthday', 'Conference', 'Party'],
     }),
     computed: {
-        ...mapState([
-            "events"
-        ]),
+        ...mapState(["events"]),// イベントデータ
     },
     methods: {
         login () {
             this.$login()
         },
+        // カレンダーデータ取得処理
         getEvents ({ start, end }) {
             const events = []
-            this.$getGapiClient().then(gapi => {
-                //console.log("aaa",this.$isSignedIn())
-                
+            // Googleカレンダー取得
+            this.$getGapiClient().then(gapi => {                
                 gapi.client.calendar.events.list({
                     calendarId: "nagami@key-p.jp",
-                    //calendarId: "ja.japanese#holiday@group.v.calendar.google.com",
-                    // 'timeMin': (new Date()).toISOString(),
-                    // 'timeMax': (date.toISOString()),
                     timeMin: (new Date(Date.parse("2020-01-01"))).toISOString(),
                     timeMax: (new Date(Date.parse("2020-06-01"))).toISOString(),
                     items: [{ "id": 'primary' }]
                 }).then(function(response) {
-                    //vm.results = response.result
-                    console.log(response.result.items)
                     var allevents = response.result.items;
                     const formatDate = (a) => {
-                        console.log("aaaa",a)
                         var start =a.dateTime
                         if(a.date){
                             start=a.date
                         }
                         return  String(start).replace("T", " ").replace("+09:00", "");
                     }
-
+                    
+                    // 取得したデータを配列にセット
                     for (let i = 0; i < allevents.length; i++) {
                         if(allevents[i].start){
                         events.push({
@@ -154,11 +134,9 @@ export default {
                         })
                         }
                     }
-                    console.log("bbb",events)
-                    const payload = {
-                        events: events,
-                    };
-                    store.commit("setEvents",payload);
+                    
+                    // ステートにイベントデータを保存
+                    store.commit("setEvents",{events: events});
                 })
             })
         },
